cbb8c3fb972361560a9b12ef9b7ea6a1fdd81863
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-09-14 13:39:11 +0300

    implemented multi threading sort without merging result

--- Lab1Task5/Lab1Task5/Lab1Task6.cpp
+++ Lab1Task5/Lab1Task5/Lab1Task6.cpp
@@ -1,22 +1,75 @@
 ﻿#include <iostream>
 #include <Windows.h>
 
-
+DWORD WINAPI threadSort(LPVOID);
 void merge(int*, int, int, int);
 void mergeSort(int*, int, int);
 int* generateArr(int);
 
+struct ThreadData {
+    int* arr;
+    int start;
+    int end;
+    int threadNum;
+    HANDLE startEvent;
+};
+
 int main() {
-    int* arr = generateArr(25);
-    for (int i = 0; i < 25; i++) {
+    int threadsCount, arrSize;
+    std::cout << "Enter threads count (from 1 to 10)\n";
+    std::cin >> threadsCount;
+    std::cout << "Enter arr size\n";
+    std::cin >> arrSize;
+    if (threadsCount < 1)
+        threadsCount = 1;
+    if (threadsCount > 10)
+        threadsCount = 10;
+
+    int* arr = generateArr(arrSize);
+
+    for (int i = 0; i < arrSize; i++) {
         std::cout << arr[i] << " ";
     }
-    std::cout << "\n\n";
-    mergeSort(arr, 0, 25 - 1);
-    for (int i = 0; i < 25; i++) {
+    std::cout << "\n";
+
+    HANDLE* threads = new HANDLE[threadsCount];
+    HANDLE startEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+    int tArrInterval = arrSize / threadsCount;
+    for (int i = 0; i < threadsCount; i++) {
+        ThreadData* tData = new ThreadData;
+        tData->arr = arr;
+        tData->start = i * tArrInterval;
+        tData->end = i != threadsCount - 1
+                    ? ((i + 1) * (tArrInterval)) - 1
+                    : arrSize - 1;
+        tData->threadNum = i + 1;
+        tData->startEvent = startEvent;
+        
+        threads[i] = CreateThread(NULL, 0, threadSort, tData, 0, NULL);
+    }
+
+    SetEvent(startEvent);
+    WaitForMultipleObjects(threadsCount, threads, TRUE, INFINITE);
+
+    for (int i = 0; i < arrSize; i++) {
         std::cout << arr[i] << " ";
     }
+    std::cout << "\n";
+
+    for (int i = 0; i < threadsCount; i++) {
+        CloseHandle(threads[i]);
+    }
+    delete[] arr;
+    delete[] threads;
 	return 0;
+}
+
+DWORD WINAPI threadSort(LPVOID tData) {
+    ThreadData* data = static_cast<ThreadData*>(tData);
+    WaitForSingleObject(data->startEvent, INFINITE);
+    mergeSort(data->arr, data->start, data->end);
+    return 0;
 }
 
 void merge(int* arr, int left, int mid, int right) {
@@ -75,7 +128,7 @@ int* generateArr(int size) {
     srand(time(0));
     int* arr = new int[size];
     for (long long i = 0; i < size; i++) {
-        arr[i] = rand();
+        arr[i] = rand() % 10;
     }
     return arr;
 }
